dd49d8f0d4a94a8dcd34b225fe2adc0a193ff845
Muieay <muieay@qq.com>
2025-09-14 13:40:39 +0800

    1

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,20 @@
+"use client"
 import Image from "next/image";
-
+import { useEffect } from "react";
 export default function Home() {
+
+  function handleClick() {
+    console.log("Clicked!");
+    fetch("/api/hello").then((res) => res.json()).then((data) => {
+      console.log(data);
+    });
+  }
+
+  // 生命周期函数
+  useEffect(() => {
+    handleClick();
+  }, []);
+
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
